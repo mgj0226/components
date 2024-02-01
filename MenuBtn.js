a9117f8c47f86c0942bca182553e4f64e119a5dd
mgj0226 <michael27mic@gmail.com>
2024-02-01 17:48:11 +0000

    darkModeSwitch is fine

--- MenuBtn.js
+++ MenuBtn.js
@@ -1,7 +1,7 @@
 const MenuBtn = ({menuBtnLightRef}) => {
     return (
         <>
-            <svg width="100" height="100" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg width="40" height="40" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="menuBtn">
                 <g id="menuBtnDark">
                 <g id="base" filter="url(#filter0_i_9_20)">
